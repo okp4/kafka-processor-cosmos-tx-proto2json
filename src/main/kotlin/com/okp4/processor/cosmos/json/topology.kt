a30afbca1cf22bf94beacd6e205fc15fae9ab083
Christophe Camel <camel.christophe@gmail.com>
2022-04-20 10:16:51 +0200

    fix: implement the TxRaw to Tx decoding

--- src/main/kotlin/com/okp4/processor/cosmos/json/topology.kt
+++ src/main/kotlin/com/okp4/processor/cosmos/json/topology.kt
@@ -1,7 +1,7 @@
 package com.okp4.processor.cosmos.json
 
-import com.google.protobuf.Any
 import com.google.protobuf.util.JsonFormat
+import cosmos.tx.v1beta1.TxOuterClass
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.Topology
@@ -27,8 +27,18 @@ fun topology(props: Properties, typeRegistry: JsonFormat.TypeRegistry): Topology
         .apply {
             stream(topicIn, Consumed.with(Serdes.String(), Serdes.ByteArray()).withName("input"))
                 .peek({ _, _ -> logger.info("Received a message") }, Named.`as`("log"))
-                .mapValues({ v -> Any.parseFrom(v) }, Named.`as`("unmarshall"))
-                .mapValues({ v -> formatter.print(v) }, Named.`as`("json-serialize"))
-                .to(topicOut, Produced.with(Serdes.String(), Serdes.String()).withName("output"))
-        }.build()
-}
+                .mapValues({ v ->
+                    val txRaw = TxOuterClass.TxRaw.parseFrom(v)
+
+                    TxOuterClass.Tx.newBuilder()
+                        .addAllSignatures(txRaw.signaturesList)
+                        .setBody(TxOuterClass.TxBody.parseFrom(txRaw.bodyBytes))
+                        .setAuthInfo(TxOuterClass.AuthInfo.parseFrom(txRaw.authInfoBytes))
+                        .build()
+                }, Named.`as`("unmarshall"))
+                    .mapValues({ it ->
+                        formatter.print(it)
+                    }, Named.`as`("serialize-json"))
+                        .to(topicOut, Produced.with(Serdes.String(), Serdes.String()).withName("output"))
+                }.build()
+        }
